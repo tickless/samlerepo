9ac0d7dc212776ae37c960d249fdbf675cf8f7e7
Alf-André Walla <fwsgonzo@hotmail.com>
2019-03-19 12:26:01 +0100

    Remove gitmodules, use direct clones

---
+++ repos.sh
@@ -0,0 +1,11 @@
+git clone git@github.com:fwsGonzo/IncludeOS.git
+git clone git@github.com:fwsGonzo/websockets.git
+git clone git@github.com:tickless/vmrunner.git
+git clone git@github.com:tickless/conan.git
+git clone git@github.com:includeos/conan_config.git
+git clone git@github.com:includeos/diskbuilder.git
+git clone git@github.com:includeos/solo5.git
+git clone git@github.com:includeos/vmbuild.git
+git clone git@github.com:fwsGonzo/musl.git
+git clone git@github.com:tickless/demo-examples.git
+git clone git@github.com:tickless/microlb.git
--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-git submodule update --init --recursive
+source repos.sh
+pushd IncludeOS
+git remote add HiOA https://github.com/hioa-cs/IncludeOS.git
+git remote add andreas git@github.com:AndreasAakesson/IncludeOS.git
+git remote add kristian git@github.com:KristianJerpetjon/IncludeOS.git
+popd
